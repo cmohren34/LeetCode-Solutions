95f8572ae569753fe3ebbb530be56d0e0aae5ab4
Carlos Moreno <40780054+cmohren34@users.noreply.github.com>
2021-08-16 17:32:09 -0700

    solution to Two Sum

--- Algorithms/Two Sum.py
+++ Algorithms/Two Sum.py
@@ -5,4 +5,10 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        for i, num1 in enumerate(nums):
+            for j, num2 in enumerate(nums):
+                if i >= j: continue
+                if num1 + num2 == target: 
+                    return[i,j]
+
+# here enumerate is used to make a tuple that gives the (index,value)
