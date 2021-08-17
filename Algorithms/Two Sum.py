7eac494b823efb026f8d401cf3ef32a8c33a011a
Carlos Moreno <40780054+cmohren34@users.noreply.github.com>
2021-08-16 17:00:57 -0700

    added comments for understanding of problem

--- Algorithms/Two Sum.py
+++ Algorithms/Two Sum.py
@@ -1,3 +1,8 @@
+# for this problem, give a list of integers from input, you have to return the 2 indices that 
+# add up to the sum of the target number (also from input)
+# for example: if you are give a list that is [2,7,11,15] and the target = 9
+#    then you would return [0,1] because 2+7 = 9
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
