a0dd883e524c7d52c452134b2ad696f17d66e084
Carlos Moreno <40780054+cmohren34@users.noreply.github.com>
2021-08-19 16:05:33 -0700

    changed last conditionals

--- Algorithms/Reverse Integer.py
+++ Algorithms/Reverse Integer.py
@@ -3,7 +3,7 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
-        # successfully returning a reveresed string greater             than 0
+        # successfully returning a reveresed string greater than 0
         str_x = str(x)
         rev_string = str_x[::-1]
         
@@ -17,6 +17,8 @@ class Solution:
         # if reversing the integer made it go outside of the given bounds
         
         if rev_x < -(2**31) or rev_x > 2**31 - 1:
+            return 0
+        elif rev_x > 2**31 - 1:
             return 0
         else:
             return rev_x
