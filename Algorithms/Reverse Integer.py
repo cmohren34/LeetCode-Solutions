35d45bdc2f549f9a1a5e80c556f9ccaa5ccbd707
Carlos Moreno <40780054+cmohren34@users.noreply.github.com>
2021-08-19 16:03:38 -0700

    completed reverse integer w/ comments

--- Algorithms/Reverse Integer.py
+++ Algorithms/Reverse Integer.py
@@ -1,4 +1,22 @@
-# in this problem you 
+# in this situtation you are given a sequence of integers and are asked to reverse them, however
+# if it excedes the given bounds you must print out a 0
 
 class Solution:
     def reverse(self, x: int) -> int:
+        # successfully returning a reveresed string greater             than 0
+        str_x = str(x)
+        rev_string = str_x[::-1]
+        
+        # if it was a negatively signed integer and it now ends in an integer after being reversed
+        
+        if rev_string[-1] == '-':
+            rev_x = -1 * int(rev_string[:-1])
+        else:
+            rev_x = int(rev_string)
+        
+        # if reversing the integer made it go outside of the given bounds
+        
+        if rev_x < -(2**31) or rev_x > 2**31 - 1:
+            return 0
+        else:
+            return rev_x
