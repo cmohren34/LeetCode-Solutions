d63ffd601c2ae9812cba5eec976f4885d9d4c1dc
Carlos Moreno <cmoren34@asu.edu>
2021-08-27 14:29:16 -0700

    Add files via upload

---
+++ Algorithms/MoveZeroes.py
@@ -0,0 +1,14 @@
+from typing import List
+
+def moveZeroes(nums: List[int]) -> None:
+    # do not return anything, modify nums in place
+
+    for x in nums:
+        if x == 0:
+            nums.remove(0)
+            nums.append(0)
+
+
+nums = [0,10,3,12]
+moveZeroes(nums)
+print(nums)
